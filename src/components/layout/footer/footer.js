bf8ca89df387710c1a422adfe56157f84cfbe522
Ange Pagel <angepagel@gmail.com>
2020-08-12 16:58:44 +0200

    Màj du Footer

--- src/components/layout/footer/footer.js
+++ src/components/layout/footer/footer.js
@@ -1,4 +1,7 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+import SocialAccounts from "../../social/socialAccounts/socialAccounts";
 
 import "./footer.scss";
 
@@ -6,8 +9,17 @@ export default class Footer extends React.Component {
     
   render() {
     return (
-      <footer>
-        Footer
+      <footer className="bg-dark text-secondary p-3">
+        <Container>
+          <Row>
+            <Col>
+              © 2020 Ange Pagel. Tous droits réservés
+            </Col>
+            <Col>
+              <SocialAccounts className="float-right" />
+            </Col>
+          </Row>
+        </Container>
       </footer>
     );
   }
