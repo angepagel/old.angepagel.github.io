349b7f141b0927923b0d3aad849e20edd0ec8ca5
Ange Pagel <angepagel@gmail.com>
2020-08-12 16:58:23 +0200

    Màj du Header

--- src/components/layout/header/header.js
+++ src/components/layout/header/header.js
@@ -1,4 +1,7 @@
 import React from "react";
+import { Link } from "gatsby";
+import { Container, Navbar, Nav } from "react-bootstrap";
+import { Menu as MenuIcon } from "react-feather";
 
 import "./header.scss";
 
@@ -7,7 +10,34 @@ export default class Header extends React.Component {
   render() {
     return (
       <header>
-        Header
+        <Navbar className="bg-dark py-3" expand="lg">
+          <Container>
+
+            <Navbar.Brand>
+              <Link to="/">Ange Pagel</Link>
+            </Navbar.Brand>
+
+            <Navbar.Toggle aria-controls="navbar-nav">
+              <MenuIcon />
+            </Navbar.Toggle>
+
+            <Navbar.Collapse id="navbar-nav">
+              <Nav className="ml-auto">
+                <Nav.Item>
+                  <Link to="/" activeClassName="nav-active">
+                    Accueil
+                  </Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Link to="/blog" activeClassName="nav-active">
+                    Blog
+                  </Link>
+                </Nav.Item>
+              </Nav>
+            </Navbar.Collapse>
+
+          </Container>
+        </Navbar>
       </header>
     );
   }
