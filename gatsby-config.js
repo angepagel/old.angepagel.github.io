c61596fc49e42c50711c21c8b5b75021e3b85bf8
Ange Pagel <angepagel@gmail.com>
2020-08-12 13:43:21 +0200

    Ajout du plugin gatsby-plugin-layout

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,12 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-plugin-layout`,
+      options: {
+        component: require.resolve(`./src/components/layout/layout.js`),
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,17 +1,16 @@
 import React from "react";
 
-import Layout from "../components/layout/layout";
 import SEO from "../components/seo";
 
 export default class BlogPage extends React.Component {
 
   render() {
     return (
-      <Layout>
+      <>
         <SEO title="Blog" />
         <h1>Blog</h1>
         <p>What happened to the blog?! :^O</p>
-      </Layout>
+      </>
     );
   }
 
