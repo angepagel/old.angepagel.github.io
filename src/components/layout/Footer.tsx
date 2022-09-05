6d9ce26311073b77b14a74a02224a9bc699fed23
Ange Pagel <angepagel@gmail.com>
2022-09-05 09:47:24 +0200

    tmp

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+// import SocialAccounts from "../../social/socialAccounts/socialAccounts";
+
+import "./footer.scss";
+
+export default class Footer extends React.Component {
+    
+  render() {
+    return (
+      <footer className="bg-dark text-secondary py-3">
+        <Container>
+          <Row>
+            <Col>
+              © 2021 Ange Pagel. Tous droits réservés.
+            </Col>
+            <Col>
+              <SocialAccounts className="float-right" />
+            </Col>
+          </Row>
+        </Container>
+      </footer>
+    );
+  }
+
+}
---
+++ src/pages/Home.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+export default function Home() {
+
+  return (
+    <>
+    
+      <Container>
+        <Row>
+          <Col>
+          </Col>
+          <Col>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+
+}
